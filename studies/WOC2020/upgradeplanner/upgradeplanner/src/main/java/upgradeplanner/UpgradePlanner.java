b4e9d6c970b9047d696d739eafb58f202eb66aa4
gbudigiri <60463007+gbudigiri@users.noreply.github.com>
2021-04-30 14:52:09 +0200

    Update UpgradePlanner.java

--- studies/WOC2020/upgradeplanner/upgradeplanner/src/main/java/upgradeplanner/UpgradePlanner.java
+++ studies/WOC2020/upgradeplanner/upgradeplanner/src/main/java/upgradeplanner/UpgradePlanner.java
@@ -10,9 +10,9 @@ import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.openapi.models.*;
-import io.kubernetes.client.util.ClientBuilder;
-import io.kubernetes.client.util.Config;
+import io.kubernetes.client.openapi.models.*; //not found
+import io.kubernetes.client.util.ClientBuilder; //not found
+import io.kubernetes.client.util.Config; //not found
 
 import java.util.*;
 import java.util.concurrent.Executors;
